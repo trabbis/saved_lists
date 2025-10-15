bb428bc495db4498c47b44441b314f5d2e073b0e
Duksun Choi <dchoi@tpl.ca>
2025-10-15 16:59:26 -0400

    adding inner join with item_lists table

--- saved-list.sql
+++ saved-list.sql
@@ -1,7 +1,14 @@
-SELECT 
-    ROW_NUMBER() OVER (ORDER BY id ASC) as row_number,
-    id, 
-    user_id 
-FROM public.lists
-ORDER BY id ASC 
+-- Getting save lists
+
+SELECT ROW_NUMBER() OVER (ORDER BY l.id ASC) as row_number,
+    l.id, 
+    l.user_id,
+    l.*,
+    COUNT(il.item_id) as item_count
+FROM public.lists l
+    INNER JOIN item_lists il on l.id = il.list_id
+GROUP BY l.id, l.user_id, l.*
+ORDER BY l.id ASC 
 LIMIT 100
+
+
