ab56fda5ff065e8398243c0573c6567dc366be2d
Duksun Choi <dchoi@tpl.ca>
2025-10-16 14:44:12 -0400

    adding items query

--- saved-list.sql
+++ saved-list.sql
@@ -1,5 +1,5 @@
 -- Getting save lists
-
+/* if requires for integer values, this might help
 SELECT ROW_NUMBER() OVER (ORDER BY l.id ASC) as row_number,
     l.id, 
     l.user_id,
@@ -10,5 +10,36 @@ FROM public.lists l
 GROUP BY l.id, l.user_id, l.*
 ORDER BY l.id ASC 
 LIMIT 100
+*/
+
+SELECT 
+   	id AS list_id,
+   	user_id AS borrower_id,
+   	REGEXP_REPLACE(name, '[,\\s]+', ' ', 'g') AS name,
+   	description,
+   	created_at AS date_created,
+   	modified_at AS date_updated,
+	CASE WHEN share_token IS NOT NULL THEN 1 ELSE 0 END AS public_list
+
+FROM public.lists
+LIMIT 100
+
+
+
+
+--TODO Getting saved items
+
+SELECT 
+	il.list_id AS list_id,
+    i.record_id AS bib_id,
+    i.created_at AS date_added
+--            i.id AS item_id,
+--	l.*, i.* 
+	FROM public.items i --taking 3 mins
+	LEFT JOIN item_lists il on i.id = il.item_id
+	LEFT JOIN lists l on l.id = il.list_id
+--	WHERE i.user_id = '4824671'  --Doug
+--ORDER BY i.id ASC LIMIT 100
+
 
 
